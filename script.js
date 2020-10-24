c1c222e71979d6409429f98029130b2eb56cfbb7
dayDream13er <73373240+dayDream13er@users.noreply.github.com>
2020-10-24 07:50:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,20 +22,20 @@ $(document).ready(function(){
 })
 
 
-// Add smooth scrolling on all links inside the navbar
+// Добавляет сглаживание на все ссылки внутри навбара
 
 $("#navbar-menu a").on('click',function(event){
     if(this.hash!==""){
         event.preventDefault();
 
         var hash = this.hash;
-        // Using jQuery's animate() method to add smooth page scroll
+        // Использует jQuery's animate() метод чтобы добавить эффект смазывание при скролле страницы(Северин, привет)
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 
         $('html,body').animate({
             scrollTop:$(hash).offset().top
         },800,function(){
-            // Add hash (#) to URL when done scrolling (default click behavior)
+          
             window.location.hash=hash;
         });
     }
